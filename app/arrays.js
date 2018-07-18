82d9fb9198a3729a1db22460a763fcd7e3e3328a
Johntejeda <Johntejeda1969@gmail.com>
2018-07-18 15:20:14 -0500

    modify arrays

--- app/arrays.js
+++ app/arrays.js
@@ -2,52 +2,93 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
   indexOf: function(arr, item) {
+     return arr.indexOf(item);
+   },
 
-  },
+   sum: function(arr) {
+       return arr.reduce(function(previousValue, currentValue) {
+         return previousValue + currentValue;
+       });
+     },
 
-  sum: function(arr) {
+     remove: function(arr, item) {
+         arr = arr.filter(function(val) {
+           return val != item;
+         });
 
-  },
-
-  remove: function(arr, item) {
-
-  },
+         return arr;
+       },
 
   removeWithoutCopy: function(arr, item) {
 
-  },
+    var arr2 = arr;
+
+      for(var i = arr2.length - 1; i >= 0; i--) {
+        if(arr2[i] === item) {
+         arr2.splice(i, 1);
+        }
+      }
+
+      return arr;
+    },
 
   append: function(arr, item) {
-
-  },
+    arr[arr.length] = item;
+  return arr;
+},
 
   truncate: function(arr) {
-
+  arr.splice(arr.length - 1, 1);
+    return arr;
   },
 
   prepend: function(arr, item) {
+  arr.unshift(item);
+      return arr;
+    },
 
-  },
-
-  curtail: function(arr) {
-
-  },
+    curtail: function(arr) {
+        arr.shift();
+        return arr;
+      },
 
   concat: function(arr1, arr2) {
+    return arr1.concat(arr2)
 
   },
 
   insert: function(arr, item, index) {
+    arr,splice(index,0,item);
+    return arr;
 
   },
 
   count: function(arr, item) {
 
+    var count = 0;
+    arr.reduce(function(previous, current) {
+      if (current == item) {
+        count++;
+      }
+    })
+
+    return count;
   },
 
   duplicates: function(arr) {
 
-  },
+    arr = arr.slice().sort();
+
+   var dup = [];
+
+   arr.filter(function(val, i) {
+     if (arr[i - 1] === val && dup.indexOf(val) === -1) {
+       dup.push(val);
+     }
+   });
+
+   return dup;
+ },
 
   square: function(arr) {
 
