4d9fb4febcf2a648f3ec6dd87d89579f36eb37a2
Johntejeda <Johntejeda1969@gmail.com>
2018-07-23 10:53:50 -0500

    worked on strings

--- app/strings.js
+++ app/strings.js
@@ -1,15 +1,36 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
-  reduceString: function(str, amount) {
-
-  },
+  rreduceString = function(str, amount) {
+  var count = 0;
+  var result = '';
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] === str[i+1]) {
+      count++;
+      if (count < amount) {
+        result += str[i];
+      }
+    } else {
+      count = 0;
+      result += str[i];
+    }
+  };
+  return result;
+}
 
   wordWrap: function(str, cols) {
 
   },
 
-  reverseString: function(str) {
+  function reverseString(str) {
 
+    var strArray = str.split("");
+    strArray.reverse();
+
+    var strReverse = strArray.join("");
+
+    return strReverse;
   }
+
+  reverseString("hello");
 };
